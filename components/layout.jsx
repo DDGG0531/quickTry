81fb046148696c951b7b77e15fa4eeebb8456ec7
JimDong <jimdong@damaiapp.com.tw>
2022-07-12 16:08:23 +0800

    basic inifnite scroll with pure js

--- components/layout.jsx
+++ components/layout.jsx
@@ -11,11 +11,12 @@ export default function Layout({ children }) {
     shallow
   )
   return (
-    <div className="p-10 bg-blue">
-      <header className="flex">
+    <div className="p-10 bg-blue h-full">
+      <header className="flex sticky top-0 bg-green">
         <div className="flex gap-1">
           <Link href={`/protected`}>Protected</Link>
           <Link href={`/unprotected`}>UnProtected</Link>
+          <Link href={`/infinite-scroll`}>無限滾動</Link>
         </div>
 
         <div className="ml-auto">
@@ -25,7 +26,7 @@ export default function Layout({ children }) {
           <div onClick={removeCurrentUser}>Logout</div>
         </div>
       </header>
-      <div className="bg-gray h-[500px]">{children}</div>
+      <div className="bg-gray">{children}</div>
     </div>
   )
 }
